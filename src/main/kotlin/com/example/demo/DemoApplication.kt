d56b990524641d9079360b719624803802447681
Azam <azam20104u@gmail.com>
2025-06-05 05:44:50 +0530

    refact: just added dummy comment

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class DemoApplication
-
+//startup of the application
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
 }
